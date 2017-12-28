80c4db9d3e7521d5a59cf6223d6516410c314afb
Louis-Jean Teitelbaum <ljt@meidosem.com>
2017-12-28 15:06:59 +0100

    Initial Commit

---
+++ Shock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shock
+//
+//  Created by Louis-Jean Teitelbaum on 28/12/2017.
+//  Copyright © 2017 Meidosem. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shock
+//
+//  Created by Louis-Jean Teitelbaum on 28/12/2017.
+//  Copyright © 2017 Meidosem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
